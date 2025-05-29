2a0fe54545c2f70c5756b32077c05289d3730a95
Pablo Dorin <dorin1979@gmail.com>
2025-05-29 14:22:45 +0530

    kata 33 Rock Paper Scissors v2

--- 33-rock paper scissors/kata-33.js
+++ 33-rock paper scissors/kata-33.js
@@ -1,28 +1,19 @@
 function rps(player1, player2){
-    let result = '';
+    let result ='';
 
     if(player1 === player2){
         result = 'Draw!';
     }
-
-    if(player1 === 'rock' && player2 === 'scissors'){
-        result = 'Player 1 won!';
-    } 
-    if(player1 === 'rock' && player2 === 'paper'){
-        result = 'Player 2 won!';
-    } 
-    if(player1 === 'scissors' && player2 === 'paper'){
-        result = 'Player 1 won!';
-    } 
-    if(player1 === 'scissors' && player2 === 'rock'){
-        result = 'Player 2 won!';
-    } 
-    if(player1 === 'paper' && player2 === 'rock'){
-    result = 'Player 1 won!';
-    } 
-    if(player1 === 'paper' && player2 === 'scissors'){
-    result = 'Player 2 won!';
-    } 
+    
+    const rules = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+    
+    if(result != 'Draw!'){
+    result = rules[player1] === player2 ? "Player 1 won!" : "Player 2 won!";
+    }
     return result;
 }
 
