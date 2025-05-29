cada14204fc36d7aa1626331d54cd63488a8ff7e
Pablo Dorin <dorin1979@gmail.com>
2025-05-29 14:24:08 +0530

    kata 33 RockPaperScissors v.2

--- 33-rock paper scissors/kata-33.js
+++ 33-rock paper scissors/kata-33.js
@@ -1,16 +1,16 @@
 function rps(player1, player2){
+    const rules = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
     let result ='';
 
     if(player1 === player2){
         result = 'Draw!';
     }
     
-    const rules = {
-        rock: "scissors",
-        paper: "rock",
-        scissors: "paper"
-    };
-    
+
     if(result != 'Draw!'){
     result = rules[player1] === player2 ? "Player 1 won!" : "Player 2 won!";
     }
