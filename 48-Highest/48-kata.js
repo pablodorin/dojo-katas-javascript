ecf08add8aca54b23508d430224943119f54b0b3
Pablo Dorin <dorin1979@gmail.com>
2025-06-21 15:20:08 +0530

    48 kata: highest

---
+++ 48-Highest/48-kata.js
@@ -0,0 +1,18 @@
+function highest(arr){
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    return [min, max];
+    
+}
+
+module.exports = highest;
