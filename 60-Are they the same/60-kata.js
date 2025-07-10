54a81db9f1f1d7afc3170560372a3223ea54866c
Pablo Dorin <dorin1979@gmail.com>
2025-07-10 13:40:00 +0200

    60-kata (2) Ther are the same

--- 60-Are they the same/60-kata.js
+++ 60-Are they the same/60-kata.js
@@ -1,24 +1,24 @@
 function comp(array1, array2) {
-    let result = true;
+  let result = true;
 
-    if (!array1 || !array2) {
-        result = false;
-    } else if (array1.length !== array2.length) {
-        result = false;
-    } else {
-        
-        let squaredArray1 = array1.map(n => n ** 2).sort((a, b) => a - b);
-        let sortedArray2 = array2.slice().sort((a, b) => a - b);
+  if (!array1 || !array2) {
+    result = false;
+  } else if (array1.length !== array2.length) {
+    result = false;
+  } else {
+    let squaredArray1 = array1.map((n) => n ** 2).sort((a, b) => a - b);
+    let sortedArray2 = array2.slice().sort((a, b) => a - b);
 
-        for (let i = 0; i < squaredArray1.length; i++) {
-            if (squaredArray1[i] !== sortedArray2[i]) {
-                result = false;
-                break;
-            }
-        }
+    let i = 0;
+    while (i < squaredArray1.length && result) {
+      if (squaredArray1[i] !== sortedArray2[i]) {
+        result = false;
+      }
+      i++;
     }
+  }
 
-    return result;
+  return result;
 }
 
 module.exports = comp;
